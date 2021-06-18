3f04d4cfb10d8256d9054de227238b40bd954a3c
Maanas Talwar <maanastalwar1@gmail.com>
2021-06-18 10:01:09 +0530

    added dynamic progress bar + connected mongoDB

--- Dashboard/app.py
+++ Dashboard/app.py
@@ -1,37 +1,49 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, Response
+import time
+from pymongo import MongoClient
+import json
 
 app = Flask(__name__)
+
+client = MongoClient('localhost', 27017)
+scan8 = client['scan8']
+prequeuedScans = scan8['prequeuedScans']
+queuedScans = scan8['queuedScans']
+runningScans = scan8['runningScans']
+completedScans = scan8['completedScans']
 
 # TODO: add a caching layer to all routes
 
 
 def index():
-    # TODO: connect mongoDB and retrieve data to lists
-
-    queued = [
-        {"scanNo": "--", "submitTime": "01-01-2000 12:00:00",
-            "size": "--", "numFiles": "--", "stats": "0"},
-        {"scanNo": "--", "submitTime": "01-02-2000 01:00:00",
-            "size": "--", "numFiles": "--", "stats": "0"},
-        {"scanNo": "--", "submitTime": "01-03-2000 02:00:00",
-            "size": "--", "numFiles": "--", "stats": "0"}
-    ]
-    running = [
-        {"scanNo": "1", "submitTime": "02-02-2001 13:00:00",
-            "size": "4GB", "numFiles": "100", "stats": "50"},
-        {"scanNo": "2", "submitTime": "03-03-2002 14:00:00",
-            "size": "1GB", "numFiles": "10", "stats": "75"}
-    ]
-    completed = [{"scanNo": "4", "submitTime": "04-04-2004 14:00:00",
-                  "size": "9GB", "numFiles": "1000", "stats": "100"}]
-
-    return render_template('index.html', queued=queued, running=running, completed=completed)
+    prequeued = prequeuedScans.find()
+    queued = queuedScans.find()
+    running = runningScans.find()
+    completed = completedScans.find()
+    return render_template('index.html', prequeued=prequeued, queued=queued, running=running, completed=completed, newScanUrl=url_for('newScan'))
 
 
 def new_scan():
     return render_template('newScan.html')
 
 
+def progress():
+    def generate():
+        while True:
+            running = runningScans.find()
+            x = {}
+            for item in running:
+                x[item["_id"]] = str(
+                    (item["files"]["completed"] / item["files"]["total"]) * 100)
+
+            yield "data:" + json.dumps(x) + "\n\n"
+            time.sleep(1)
+
+    return Response(generate(), mimetype='text/event-stream')
+
+
 app.add_url_rule("/", endpoint="dashboard", view_func=index, methods=['GET'])
 app.add_url_rule("/newScan", endpoint="newScan",
                  view_func=new_scan, methods=['GET'])
+app.add_url_rule("/progress", endpoint="progress",
+                 view_func=progress, methods=['GET'])
