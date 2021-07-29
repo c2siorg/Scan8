b507841c821c12d4d3507fd4f7b48ad47048d850
Sarthak Singh <sarthaksingh0900@gmail.com>
2021-07-29 23:32:29 +0530

    refactoring file names

--- Dockerfiles/Dashboard+Coordinator/entryPoint.sh
+++ Dockerfiles/Dashboard+Coordinator/entryPoint.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-if [ -d "/app/common/Results" ]
+
+#checking if directory exists or not
+if [ ! -d "/app/common/Results" ]
 then 
     mkdir -p /app/common/Uploads
     mkdir -p /app/common/Results    
 fi
+
 #Running Flask Web Server
+
 cd Dashboard
 export LC_ALL=C.UTF-8
 export LANG=C.UTF-8
@@ -13,6 +17,7 @@ nohup python3 app.py &
 cd ..
 
 #Running Coordinator node
+
 cd Coordinator
 nohup python3 app.py &
 cd ..
