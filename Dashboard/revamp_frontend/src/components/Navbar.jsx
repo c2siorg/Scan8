b902e5739d3c9eb5e62c48c4c46cdc3e2c68df18
Shelly011s <shellysuthar.ict19@gmail.com>
2023-06-25 12:24:37 +0530

    added a modal to upload files

--- Dashboard/revamp_frontend/src/components/Navbar.jsx
+++ Dashboard/revamp_frontend/src/components/Navbar.jsx
@@ -1,11 +1,11 @@
 import { Navbar, Container, Button }from 'react-bootstrap';
 
-const NavBar = () =>{
+const NavBar = ({modal,setModal}) =>{
     return(
         <Navbar bg="dark" data-bs-theme="dark">
         <Container>
           <Navbar.Brand className="fs-2 fw-bold">Scan8</Navbar.Brand>
-          <Button className="fw-bold" variant="outline-success">New Scan</Button>
+          <Button className="fw-bold" variant="outline-success" onClick={() => setModal(!modal)}>New Scan</Button>
         </Container>
       </Navbar>
     );
