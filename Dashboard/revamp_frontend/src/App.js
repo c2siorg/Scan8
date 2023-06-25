b91862492f953029193e5bfc0545346edf78a979
Shelly011s <shellysuthar.ict19@gmail.com>
2023-06-25 21:34:37 +0530

    Created the scan component and added style

--- Dashboard/revamp_frontend/src/App.js
+++ Dashboard/revamp_frontend/src/App.js
@@ -1,11 +1,41 @@
 import { useEffect, useState } from "react";
 import { Container } from "react-bootstrap";
 import NavBar from "./components/Navbar";
+import io from "socket.io-client";
+import Scan from "./components/Scan";
+import ScanModal from "./components/Modal";
 
 function App() {
-    return (
+  //const flaskUrl = process.env.REACT_APP_FLASK_URL;
+  const [data, setData] = useState({ queued: [], running: [], completed: [] });
+  const [modal, setModal] = useState(false);
+
+  return (
     <div>
-      <NavBar />
+      <NavBar modal={modal} setModal={setModal} />
+      <Container className="d-flex text-muted mt-3">
+        <p className="scan-header">Scan</p>
+        <div className="d-flex justify-content-between">
+          <p>Size</p>
+          <p>Number of Files</p>
+          <p>Submitted</p>
+        </div>
+      </Container>
+      {data ? JSON.stringify(data) : "Data"}
+      <Container className="bg-white mx-auto p-0">
+        <h4>Queued Scans</h4>
+        {data.queued.length > 0 &&
+          data.queued.map((item) => <Scan item={item} variant={"warning"} />)}
+        <h4>Running Scans</h4>
+        {data.running.length > 0 &&
+          data.running.map((item) => <Scan item={item} variant={"info"} />)}
+        <h4>Completed Scans</h4>
+        {data.completed.length > 0 &&
+          data.completed.map((item) => (
+            <Scan item={item} variant={"success"} />
+          ))}
+      </Container>
+      <ScanModal modal={modal} setModal={setModal} />
     </div>
   );
 }
--- Dashboard/revamp_frontend/src/components/Scan.jsx
+++ Dashboard/revamp_frontend/src/components/Scan.jsx
@@ -1,33 +1,96 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faArrowsRotate,
+  faCheck,
+  faExclamation,
+  faPause,
+} from "@fortawesome/free-solid-svg-icons";
 import { Container, ProgressBar } from "react-bootstrap";
 
+const timeAgo = (date) => {
+  const seconds = Math.floor((new Date() - date) / 1000);
+
+  let interval = Math.floor(seconds / 31536000);
+  if (interval > 1) {
+    return interval + " years ago";
+  }
+
+  interval = Math.floor(seconds / 2592000);
+  if (interval > 1) {
+    return interval + " months ago";
+  }
+
+  interval = Math.floor(seconds / 86400);
+  if (interval > 1) {
+    return interval + " days ago";
+  }
+
+  interval = Math.floor(seconds / 3600);
+  if (interval > 1) {
+    return interval + " hours ago";
+  }
+
+  interval = Math.floor(seconds / 60);
+  if (interval > 1) {
+    return interval + " minutes ago";
+  }
+
+  if (seconds < 10) return "just now";
+
+  return Math.floor(seconds) + " seconds ago";
+};
+
+const Icon = ({ status }) => {
+  switch (status) {
+    case "warning":
+      return <FontAwesomeIcon icon={faPause} style={{ color: "#ffffff" }} />;
+    case "info":
+      return <FontAwesomeIcon icon={faArrowsRotate} style={{ color: "#ffffff" }} />;
+    case "success":
+      return <FontAwesomeIcon icon={faCheck} style={{ color: "#ffffff" }} />;
+    case "danger":
+      return <FontAwesomeIcon icon={faExclamation} style={{ color: "#ffffff" }} />;
+    default:
+      return null;
+  }
+};
+
 const Scan = ({ item, variant }) => {
+  const date = timeAgo(
+    new Date(
+      item.submitTime.date.split("-").reverse().join("-") +
+        " " +
+        item.submitTime.time
+    )
+  );
   const now =
     variant === "info"
       ? Math.floor((item["files"]["completed"] / item["files"]["total"]) * 100)
       : 100;
   return (
     <Container className="scan d-flex text-muted px-4 py-2">
-      <div className="d-flex flex-basis">
-        <div
-          className={`circle d-flex align-items-center justify-content-center rounded-circle bg-${variant} mt-1 me-3`}
-        >
-          Icon
-        </div>
-        <div className="w-100">
-          <p>ID: Scan</p>
-          <ProgressBar
-            id={item._id}
-            className="w-100 mt-0 h-25"
-            variant={variant}
-            now={now}
-            label={`${now}%`}
-          />
-        </div>
+      <div
+        className={`circle d-flex align-items-center justify-content-center rounded-circle bg-${variant} mt-1 me-3`}
+      >
+        <Icon status={variant} />
       </div>
-      <p>{item.size}</p>
-      <p>{item.files.total}</p>
-      <p>{item.submiTime.date}</p>
+
+      <div className="scan-progress">
+        <p>ID: Scan</p>
+        <ProgressBar
+          id={item._id}
+          className="w-100 mt-0 h-25"
+          variant={variant}
+          now={now}
+          label={`${now}%`}
+        />
+      </div>
+      <div className="scan-details">
+        <p>{item.size}</p>
+        <p>{item.files.total}</p>
+        <p>{date}</p>
+      </div>
     </Container>
   );
 };
