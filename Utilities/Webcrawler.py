b0fe498948557ce6098ffae38e0fa7688ede0e20
Atharva Karambe <58180297+Atharva-karambe@users.noreply.github.com>
2023-06-28 20:55:17 +0530

    Webcrawler (#116)
    * Integrate WebCrawerl module, Solved all Bugs, updated front-end
    
    * Made Scan result visible
    
    * Made Scan file Delete
    
    * Update requiremen.txt and removed unwanted import

---
+++ Utilities/Webcrawler.py
@@ -0,0 +1,96 @@
+import os
+import json
+import re
+import requests
+import time
+from datetime import datetime
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin, urlparse
+
+
+# WebCrawler
+class WebCrawler:
+    def download_file(self, url, folder):
+        try:
+            # Check if URL is valid
+            response = requests.head(url, timeout=2)
+            response.raise_for_status()  # Raises an exception if the status is not 2xx
+
+            # Create the folder if it doesn't exist
+            if not os.path.exists(folder):
+                os.makedirs(folder)
+
+            # Extract the filename and extension from the URL
+            parsed_url = urlparse(url)
+            filename = os.path.basename(parsed_url.path)
+
+            # Generate a unique filename with correct extension
+            timestamp = str(int(time.time() * 1000))  # Use timestamp for uniqueness
+            save_path = os.path.join(folder, timestamp)
+
+            # Download the file
+            with requests.get(url, stream=True) as response:
+                response.raise_for_status()
+                with open(save_path, 'wb') as file:
+                    for chunk in response.iter_content(chunk_size=8192):
+                        file.write(chunk)
+
+            print(f"File downloaded successfully! Saved as {save_path}")
+        except requests.exceptions.RequestException as e:
+            print(f"An error occurred: {e}")
+
+    def extract_urls_from_js(self, url):
+        try:
+            response = requests.get(url)
+            if response.status_code == 200:
+                js_content = response.text
+                urls = self.get_urls_from_js(js_content)
+                return urls
+            else:
+                print(f'Error: Failed to fetch the URL ({url})')
+                return []
+        except requests.exceptions.RequestException as e:
+            print(f"An error occurred: {e}")
+            return []
+
+    def get_urls_from_js(self, js_code):
+        urls = re.findall(r'(?:https?:\/\/|www\.)[\w\.-]+\.[\w\.-]+[^\s\)\}\]\;\'\"]*', js_code)
+        processed_urls = {url if url.startswith('http') else 'http://' + url for url in urls}
+        return processed_urls
+
+    def extract_urls(self, soup, tag, attribute, url):
+        urls = set()
+        for link in soup.find_all(tag):
+            href = link.get(attribute)
+            if href:
+                urls.add(urljoin(url, href))
+                if href.endswith('.js'):
+                    urls.update(self.extract_urls_from_js(urljoin(url, href)))
+        return urls
+
+    # Function to retrieve URLs from a web page
+    def get_urls(self, url):
+        headers = {'User-Agent': 'Mozilla/5.0'}  # Add user agent header
+
+        with requests.Session() as session:
+            try:
+                response = session.get(url, headers=headers)
+                response.raise_for_status()
+                html = response.text
+                soup = BeautifulSoup(html, 'html.parser')
+                urls = set()
+
+                # Extract URLs from <> tags
+                for attribute in ['href', 'src']:
+                    for tag in ['a', 'link', 'img', 'area', 'base', 'script', 'iframe', 'form', 'button', 'input']:
+                        urls.update(self.extract_urls(soup, tag, attribute, url))
+
+                return urls
+
+            except requests.exceptions.RequestException as e:
+                print(f"Error: {e}")
+                return []
+
+
+
+    
--- Utilities/scanJob.py
+++ Utilities/scanJob.py
@@ -1,12 +1,16 @@
+from Webcrawler import WebCrawler
 from pymongo import MongoClient
 from redis import Redis
 import clamd
 import os
-from dotenv import load_dotenv
 import json
+from dotenv import load_dotenv
+from hurry.filesize import size, si
+from datetime import datetime
+import shutil
 
 load_dotenv()
-
+upload_path = os.getenv("UPLOAD_DIRECTORY")
 resultsPath = os.getenv("RESULTS_PATH")
 cd = clamd.ClamdUnixSocket()
 
@@ -15,11 +19,37 @@ mongodbPort = int(os.getenv("MONGODB_PORT"))
 
 client = MongoClient(mongodbHost, mongodbPort)
 scan8 = client['scan8']
+runninglinks = scan8['runninglinks']
+prequeuedScans = scan8['prequeuedScans']
 queuedScans = scan8['queuedScans']
 runningScans = scan8['runningScans']
 completedScans = scan8['completedScans']
 
 redis_client = Redis(host=os.getenv('REDIS_HOST'),port=int(os.getenv("REDIS_PORT")))
+
+def webcrawler(url,folder):
+	try:
+		crawler = WebCrawler()
+		urls = crawler.get_urls(url)
+		urls.add(url)
+		for url in urls:
+		    print(url)
+		    crawler.download_file(url, upload_path + "/"+ folder)
+		curTime = datetime.now()
+		dirSize = 0
+		numFiles = 0
+		for element in os.scandir(upload_path + "/"+ folder):
+			dirSize+=os.path.getsize(element)
+			numFiles += 1
+		runninglinks.delete_one({"_id": folder})	
+		scan8.prequeuedScans.insert_one(
+		    {"_id": str(folder), "submitTime": {"date": curTime.strftime("%d-%m-%Y"), "time": curTime.strftime(
+		        "%H:%M:%S")}, "size": size(dirSize, system=si), "files": {"total": numFiles, "completed": 0}, "result": {"Virus": 0, "Virus_name": []}}
+		)
+	except:
+		print("Taking too long to download files")
+		runninglinks.delete_one({"_id": folder})	
+
 
 # RQ job
 def scan(filePath):
@@ -34,6 +64,14 @@ def scan(filePath):
         redis_client.publish('scan_progress', json.dumps({ 'queued' : _queued, 'running': _running }))
     
     result = cd.scan(filePath)
+    result = (result[(list(result.keys())[0])])
+    if result[0] == "FOUND":
+    	data = list(runningScans.find({"_id": id}))
+    	runningScans.update_one({"_id": id}, {"$set": {'result.Virus': (((data[0])['result'])['Virus']) + 1}})
+    	result_list=(((data[0])['result'])['Virus_name'])
+    	result_list.append(result[1])
+    	runningScans.update_one({"_id": id}, {"$set": {'result.Virus_name': result_list }})
+    	
     filename = id+"_"+name+"_"+".json"
     filename = resultsPath+"/"+filename
     with open(filename, "a+") as file:
@@ -45,6 +83,7 @@ def scan(filePath):
     if(len(running) != 0 and running[0]['files']['total'] == running[0]['files']['completed']):
         completedScans.insert_one(running[0])
         runningScans.delete_one({"_id": id})
+        shutil.rmtree(upload_path + "/" + id)
 
         _running = list(runningScans.find())
         _completed = list(completedScans.find({"_id": id}))
